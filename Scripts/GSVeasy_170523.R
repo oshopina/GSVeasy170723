7d64a25bf4476c7239b8287e1fffe5d157a4dd87
Olga Shopina <uqoshopi@uq.edu.au>
2023-06-23 14:49:45 +1000

    Trying to figure out what to do with missing samples

--- Scripts/GSVeasy_170523.R
+++ Scripts/GSVeasy_170523.R
@@ -4,10 +4,10 @@ library(gsveasyr)
 env.all = read.table('Data/mag_env_for_shotgun_samples.csv',
                       header=TRUE, sep = ",")
 
-## Check if number of samples in one of the tables is not correct
-# gene.list = read.table("N_cycle/graftm_gene_counts.tsv", header = T)
+# # Check if number of samples in one of the tables is not correct
+# gene.list = read.table("Data/N_cycle/graftm_gene_counts.tsv", header = T)
 # gene.list$SampleId = str_extract(gene.list$SampleId, "(?<=_)[A-Za-z0-9]+")
-# setdiff(gene.list$SampleId, env.all$SampleID)
+# setdiff(rownames(gene.list), rownames(env.all))
 
 
 rownames(env.all) = env.all$SampleID
@@ -20,3 +20,8 @@ load_all_gsv_output(c_folder = C_folder, n_folder = N_folder, s_folder = S_folde
                     min_num_reads = 1000000, clustlvls=c(65,75,85),
                     changeSampleName = T, env_df = env.all,
                     refColumn = env.all$SampleID)
+
+bargraph.CI(GSV_gene_count_list$env$pH,
+            GSV_gene_count_list$C_raw$ackA,
+            legend = T, xlab = '', ylab = 'Gene count per million reads',
+            main = 'aprA')
